eed63a41c26e0b797eda2caa63043681cd7ccd57
GitStudentSem <programmSem@yandex.ru>
2021-03-22 09:57:57 +0300

    create advantage section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Бургер меню
+// Burger menu
 var burger = document.querySelector(".burger");
 burger.addEventListener("click", function () {
   document
@@ -6,25 +6,29 @@ burger.addEventListener("click", function () {
     .classList.toggle("header__navigation-visible");
 });
 
-// Cвайвер
-const swiper = new Swiper(".swiper-container", {
+// Company swiper
+const company = new Swiper(".company__container", {
+  grabCursor: true, // Показывает руку для перемещеня слайдов
+  slideToClickedSlide: false, // При нажатии на слайд делает его активным
   spaceBetween: 20,
-  loop: true,
+  speed: 1000,
+  loop: false,
 
-  // Navigation arrows
   navigation: {
     nextEl: ".company__button-next",
     prevEl: ".company__button-prev",
   },
 
-  // If we need pagination
   pagination: {
     clickable: true,
-    el: ".swiper-pagination",
+    el: ".company__pagination",
   },
+
   breakpoints: {
     0: {
       slidesPerView: 1,
+      //slidesPerColumnFill: "column", //как слайды должны заполнять строки, по столбцу или по строке
+      //slidesPerColumn: 1, //Количесвтво слайдов в столбце
     },
     500: {
       slidesPerView: 2,
